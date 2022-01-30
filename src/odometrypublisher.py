4bb6fd42ca51890fa26b9ff435cf77463a773e9a
silverjoda <heizen.sh@gmail.com>
2022-01-30 20:10:10 +0100

    Continue debug

--- src/odometrypublisher.py
+++ src/odometrypublisher.py
@@ -18,6 +18,7 @@ class OdometryPublisher:
 
     def init_ros(self):
         rospy.init_node("odometry_publisher")
+        print("Starting odometry publisher node...")
 
         self.tfBuffer = tf2_ros.Buffer()
         self.tflistener = tf2_ros.TransformListener(self.tfBuffer)
@@ -53,13 +54,15 @@ class OdometryPublisher:
                 self.ros_rate.sleep()
                 continue
             else:
-                wheel_speed, dv_imu, accel_imu, quat_imu = ret
+                wheel_speed, accel_imu, quat_imu = ret
+
             ctr +=1
             g_cum += accel_imu[2]
             if ctr > 100:
                 avg_g = g_cum / float(ctr)
                 print("Current g is {}".format(avg_g))
                 return avg_g
+            self.ros_rate.sleep()
 
     def loop(self):
         # Take average of gravitational acceleration
@@ -72,7 +75,10 @@ class OdometryPublisher:
                 self.ros_rate.sleep()
                 continue
             else:
-                wheel_speed, _, accel_imu, quat_imu = ret
+                wheel_speed, accel_imu, quat_imu = ret
+
+            # TODO: rename all quaternions to from_to_ notation for clarity
+            # TODO: Continue correcting and checking with rviz
 
             # Transform imu accel to imu_zrp frame
             r, p, y = euler_from_quaternion(quat_imu)
@@ -100,10 +106,13 @@ class OdometryPublisher:
             self.p_odom[1] += self.v_odom[1] * self.dt
 
             # Publish tfs
-            odom_tf = make_tf(frame="odom", child="base_link_zrp", pos=self.p_odom, q=quat_yaw_odom)
+            odom_tf = make_tf(frame="odom", child="base_link_zrp", pos=[0,0,0], q=quat_yaw_odom)
             self.broadcaster.sendTransform(odom_tf)
 
-            imu_zrp_tf = make_tf(frame="odom", child="imu_zrp", pos=self.p_odom, q=quat_imu_zrp)
+            imu_zrp_tf = make_tf(frame="odom", child="imu ", pos=[0, 0, 0], q=quat_imu) # WRONG, need to transform by pi/2 first
+            self.broadcaster.sendTransform(imu_zrp_tf)
+
+            imu_zrp_tf = make_tf(frame="imu_zrp", child="imu", pos=[0,0,0], q=quat_imu_zrp)
             self.broadcaster.sendTransform(imu_zrp_tf)
 
             base_link_tf = make_tf(frame="base_link_zrp", child="base_link", pos=[0,0,0], q=quat_bl_zrp)
@@ -124,20 +133,19 @@ class OdometryPublisher:
         pass
 
     def read_sensor_values(self):
-        with self.wheel_speed_sub.lock:
-            if self.wheel_speed_sub.msg is None: return None
-            wheel_speed = self.wheel_speed_sub.msg.data
-        with self.dv_sub.lock:
-            if self.dv_sub.msg is None: return None
-            dv = vector3tonumpy(self.dv_sub.msg.vector)
-        with self.imu_sub.lock:
-            if self.imu_sub.msg is None: return None
-            imu_accel = vector3tonumpy(self.dv_sub.msg.linear_acceleration)
-        with self.quat_sub.lock:
-            if self.quat_sub.msg is None: return None
-            quat = quaterniontonumpy(self.quat_sub.msg.quaternion)
+        msg = self.wheel_speed_sub.get_msg(copy_msg=True)
+        if msg is None: return None
+        wheel_speed = self.wheel_speed_sub.msg.data
 
-        return wheel_speed, dv, imu_accel, quat
+        msg = self.imu_sub.get_msg(copy_msg=True)
+        if msg is None: return None
+        imu_accel = vector3tonumpy(self.imu_sub.msg.linear_acceleration)
+
+        msg = self.quat_sub.get_msg(copy_msg=True)
+        if msg is None: return None
+        quat = quaterniontonumpy(self.quat_sub.msg.quaternion)
+
+        return wheel_speed, imu_accel, quat
 
 
 if __name__ == "__main__":
