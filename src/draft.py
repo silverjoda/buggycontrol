004da4e250a8d053109c6ebbcc8d9c7a6f0bb7c2
Teymur Azayev <azayetey@fel.cvut.cz>
2022-02-01 15:08:10 +0100

    Finish differentiable cal (untested)

--- src/draft.py
+++ src/draft.py
@@ -1,41 +1,18 @@
-#import torch as T
+import torch as T
 from tf.transformations import *
 import numpy as np
-T = None
-#mat_tx_no_yaw = T.tensor([[1,0,0],[0,1,0],[0,0,1]], requires_grad=True)
+mat_tx_no_yaw = T.rand(3, 3, requires_grad=True)
 
-max_iters = 1000
-for i in range(max_iters):
+for i in range(10):
     # generate random r,p,y
-    r, p, y = np.random.rand(3) - 0.5
+    r, p, y = np.random.rand(3) * 2 - 1.0
 
     # generate matrix from rpy and rp0
-    mat_rpy = euler_matrix(r, p, y)[:3, :3]
-    mat_rp0 = euler_matrix(r, p, 0)
+    mat_rpy = euler_matrix(r, p, y)[:3, :3].astype(np.float32)
+    mat_rp0 = euler_matrix(r, p, 0)[:3, :3].astype(np.float32)
 
-    # generate rp0 matrix using y_n_mat
-    mat_rp0_pred = T.matmul(mat_tx_no_yaw, T.from_numpy(mat_rpy))
-
-    # loss
-    loss = T.pow(T.from_numpy(mat_rp0) - mat_rp0_pred, 2)
-
-    # backprop
-    loss.backward()
-
-    # apply grad
-    with T.no_grad:
-        mat_tx_no_yaw -= mat_tx_no_yaw.grad * 0.01
-
-    # Zero grad
-    mat_tx_no_yaw.grad.fill_(0)
-
-    if i % 10 == 0:
-        print("Iter: {}/{}, loss: {}".format(i, max_iters, loss.data))
-
-print("Result:")
-print(mat_tx_no_yaw)
-print("Done.")
-
+    T_mat = np.matmul(mat_rp0, np.linalg.inv(mat_rpy))
+    print(T_mat)
 
 
 
--- src/odometrycalibrator.py
+++ src/odometrycalibrator.py
@@ -3,12 +3,12 @@ import pickle
 import numpy as np
 import os
 import tf2_ros
-from tf.transformations import *
+from tf.transformations import quaternion_matrix
 
 class Odometrycalibrator:
     def __init__(self):
-        list_of_dataset_dict_lists = self.load_datasets()
-        self.episodes = len(self.list_of_dataset_dict_lists)
+        list_of_dataset_dict_lists = self.load_dataset()
+        self.episodes = len(list_of_dataset_dict_lists)
         self.calibrate_using_positional_constraints(list_of_dataset_dict_lists)
 
     def load_dataset(self):
@@ -39,15 +39,46 @@ class Odometrycalibrator:
         update_val = np.minimum(target - val, tau) * sign - np.minimum(val - target, tau) * (not sign)
         return val + update_val
 
+    def remove_yaw_from_matrix_tensor(self, m):
+        yaw_mat = self.get_yaw_mat_from_matrix_tensor(m)
+        no_yaw_mat = T.matmul(yaw_mat.inverse(), m)
+        return no_yaw_mat
+
+    def get_yaw_mat_from_matrix_tensor(self, m):
+        # a, b, c, d, e, f
+        # 0, 1, 2, 0, 1, 2
+        # bf-ce, cd-af, ae-bd
+        # 12-21, 20-02, 01-10
+
+        # Project to plane
+        proj_mat = T.matmul(m, T.tensor([[1, 0], [0, 1], [0, 0]]))
+
+        # Get individual vectors
+        x = proj_mat[:, 0]
+        y = proj_mat[:, 1]
+
+        # Take cross product
+        z = T.tensor([x[1] * y[2] - x[2] * y[1],
+                      x[2] * y[0] - x[0] * y[2],
+                      x[0] * y[1] - x[1] * y[0]])
+
+        yaw_mat = T.cat([proj_mat, z], dim=1)
+
+        return yaw_mat
+
     def calibrate_using_positional_constraints(self, list_of_dataset_dict_lists):
         # Non-optimizable params:
         dt = 0.005
+
+        # Lossfuns
+        mse_loss = T.nn.MSELoss()
 
         # Define optimization vars, etc.
         wheel_speed_scalar = T.tensor(1.0, requires_grad=True)
         bl_zrp_to_imu_zrp_corr_mat = T.tensor([[1.,0.,0.],
                                                [0.,1.,0.],
                                                [0.,0.,1.]], requires_grad=True)
+
         x_vel_tau = T.tensor(0.002, requires_grad=True)
         y_vel_tau = T.tensor(0.003, requires_grad=True)
         x_accel_linear_correction = T.tensor([0.0, 1.0], requires_grad=True)
@@ -56,7 +87,6 @@ class Odometrycalibrator:
 
         # XY integrated vel and pos
         v_bl_zrp = T.tensor([0.0, 0.0], requires_grad=True)
-        v_odom = T.tensor([0.0, 0.0], requires_grad=True)
         p_odom = T.tensor([0.0, 0.0], requires_grad=True)
 
         for d in list_of_dataset_dict_lists[0]:
@@ -64,10 +94,32 @@ class Odometrycalibrator:
             wheel_speed, accel_imu, ori_imu = self.dict_to_arr_data(d)
 
             # Transform accel_imu to imu_zrp frame
-            accel_imu_zrp = None
+            accel_imu_zrp = T.matmul(accel_imu, self.remove_yaw_from_matrix_tensor(ori_imu))
 
             # Transform accel_imu_zrp to bl_zrp
+            accel_bl_zrp = T.matmul(bl_zrp_to_imu_zrp_corr_mat, accel_imu_zrp)
 
             # Update vel in bl_zrp using acceleration, wheel speed and tau
             v_bl_zrp[0] = self.update_towards(v_bl_zrp[0], wheel_speed * wheel_speed_scalar, x_vel_tau) + accel_bl_zrp[0] * dt
             v_bl_zrp[1] = self.update_towards(v_bl_zrp[1], 0., y_vel_tau) + accel_bl_zrp[1] * dt
+
+            v_odom = T.matmul(self.get_yaw_mat_from_matrix_tensor(ori_imu), v_bl_zrp)
+            p_odom += v_odom * dt
+
+        # Define losses
+        pos_loss = mse_loss(p_odom[:2], T.tensor([0., 0.]))
+        decay_losses = T.pow(x_vel_tau, 2) * 0.01 + T.pow(y_vel_tau, 2) * 0.01
+        loss_sum = pos_loss + decay_losses
+        loss_sum.backward()
+
+        # Apply grads
+        wheel_speed_scalar -= wheel_speed_scalar.grad * 0.01
+        bl_zrp_to_imu_zrp_corr_mat -= bl_zrp_to_imu_zrp_corr_mat.grad * 0.01
+        x_vel_tau -= x_vel_tau.grad * 0.01
+        y_vel_tau -= y_vel_tau.grad * 0.01
+
+        # Zero grads
+        wheel_speed_scalar.grad.fill_(0)
+        bl_zrp_to_imu_zrp_corr_mat.grad.fill_(0)
+        x_vel_tau.grad.fill_(0)
+        y_vel_tau.grad.fill_(0)
