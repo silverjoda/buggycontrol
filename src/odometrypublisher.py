a0ed723c130010c1943dc042ef29ea304c5e2047
silverjoda <heizen.sh@gmail.com>
2022-02-02 15:40:46 +0100

    Make changes to odom node

--- src/odometrypublisher.py
+++ src/odometrypublisher.py
@@ -26,7 +26,7 @@ class OdometryPublisher:
 
         self.wheel_speed_sub = subscriber_factory("/wheel_speed", Float64)
         self.dv_sub = subscriber_factory("/imu/dv", Vector3Stamped)
-        self.imu_sub = subscriber_factory("/imu/data", Imu)
+        self.accel_sub = subscriber_factory("/imu/acceleration", Vector3Stamped)
         self.quat_sub = subscriber_factory("/filter/quaternion", QuaternionStamped)
 
         self.v_bl_zrp = [0,0,0]
@@ -37,10 +37,9 @@ class OdometryPublisher:
         time.sleep(0.5)
 
     def define_calibration_params(self):
-        self.grav_accel = 9.81
-        self.bl_to_imu = quaternion_from_euler(0, 0, -np.pi / 2)
-        self.x_vel_tau = 0.002
-        self.y_vel_tau = 0.002
+        self.bl_to_imu = quaternion_from_euler(0, 0, 0)
+        self.x_vel_tau = 0.03
+        self.y_vel_tau = 3.
         self.wheel_speed_scalar = 1.0
         self.dt = 0.005
 
@@ -65,10 +64,6 @@ class OdometryPublisher:
             self.ros_rate.sleep()
 
     def loop(self):
-        # Take average of gravitational acceleration
-        #self.grav_accel = self.get_avg_g()
-        self.grav_accel = 9.81
-
         while not rospy.is_shutdown():
             # Read current sensor values (wheel, imu, quat)
             ret = self.read_sensor_values()
@@ -78,25 +73,35 @@ class OdometryPublisher:
             else:
                 wheel_speed, accel_imu, q_imu_imuinit = ret
 
+            if not hasattr(self, "prev_imu_accel_stamp"):
+                dt = self.dt
+            else:
+                dt = self.get_dt(self.imu_accel_stamp, self.prev_imu_accel_stamp)
+            self.prev_imu_accel_stamp = self.imu_accel_stamp
+
             # Transform imu accel to imu_zrp frame
             r_imu_imuinit, p_imu_imuinit, y_imu_imuinit = euler_from_quaternion(q_imu_imuinit)
             q_imu_imuzrp = quaternion_from_euler(r_imu_imuinit, p_imu_imuinit, 0)
             accel_imu_zrp = self.rotate_vector_by_quat(accel_imu, q_imu_imuzrp)
 
             # Transform (rotate) imu_accel from imu_zrp to bl_zrp
-            accel_bl_zrp = self.rotate_vector_by_quat(accel_imu_zrp, self.bl_to_imu)
+            #accel_bl_zrp = self.rotate_vector_by_quat(accel_imu_zrp, self.bl_to_imu)
+            accel_bl_zrp = accel_imu_zrp
 
             # Update vel in bl_zrp using acceleration, wheel speed and decay #
-            self.v_bl_zrp[0] = self.update_towards(self.v_bl_zrp[0], wheel_speed * self.wheel_speed_scalar, self.x_vel_tau) + accel_bl_zrp[0] * self.dt
-            self.v_bl_zrp[1] = self.update_towards(self.v_bl_zrp[1], 0., self.y_vel_tau) + accel_bl_zrp[1] * self.dt
+            self.v_bl_zrp[0] = self.update_towards(self.v_bl_zrp[0], 0., self.x_vel_tau * dt) + accel_bl_zrp[0] * dt
+            self.v_bl_zrp[1] = self.update_towards(self.v_bl_zrp[1], 0., self.y_vel_tau * dt) + accel_bl_zrp[1] * dt
 
             # Transform vel from bl_zrp to odom using quat in odom
             q_blzrp_odom = quaternion_from_euler(0, 0, y_imu_imuinit)
             self.v_odom = self.rotate_vector_by_quat(self.v_bl_zrp, q_blzrp_odom)
 
+            #self.v_odom[0] += (accel_imu[0] * dt)
+            #self.v_odom[1] += (accel_imu[1] * dt)
+
             # Update pos in odom using vel in odom
-            self.p_odom[0] += self.v_odom[0] * self.dt
-            self.p_odom[1] += self.v_odom[1] * self.dt
+            self.p_odom[0] += (self.v_odom[0] * dt)
+            self.p_odom[1] += (self.v_odom[1] * dt)
 
             # Publish tfs
             odom_tf = make_tf(frame="odom", child="base_link_zrp", pos=self.p_odom, q=q_blzrp_odom)
@@ -111,9 +116,14 @@ class OdometryPublisher:
             self.ros_rate.sleep()
 
     def update_towards(self, val, target, tau):
-        sign = val < target
-        update_val = np.minimum(target - val, tau) * sign - np.minimum(val - target, tau) * (not sign)
-        return val + update_val
+        if abs(tau) < 0.000001:
+            return val
+        if val < target:
+            return np.minimum(val + tau, target)
+        elif val > target:
+            return np.maximum(val - tau, target)
+        else:
+            return val
 
     def rotate_vector_by_quat(self, v, q):
         qm = quaternion_matrix(q)[:3, :3]
@@ -122,21 +132,26 @@ class OdometryPublisher:
     def gather(self):
         pass
 
+    def get_dt(self, stamp_new, stamp_old):
+        dt = stamp_new.secs - stamp_old.secs
+        dt += (stamp_new.nsecs - stamp_old.nsecs) / 1000000000.
+        return dt
+
     def read_sensor_values(self):
         msg = self.wheel_speed_sub.get_msg(copy_msg=True)
         if msg is None: return None
-        wheel_speed = self.wheel_speed_sub.msg.data
+        wheel_speed = msg.data
 
-        msg = self.imu_sub.get_msg(copy_msg=True)
+        msg = self.accel_sub.get_msg(copy_msg=True)
         if msg is None: return None
-        imu_accel = vector3tonumpy(self.imu_sub.msg.linear_acceleration)
+        imu_accel = vector3tonumpy(msg.vector)
+        self.imu_accel_stamp = msg.header.stamp
 
         msg = self.quat_sub.get_msg(copy_msg=True)
         if msg is None: return None
-        quat = quaterniontonumpy(self.quat_sub.msg.quaternion)
+        quat = quaterniontonumpy(msg.quaternion)
 
         return wheel_speed, imu_accel, quat
-
 
 if __name__ == "__main__":
     OdometryPublisher()
