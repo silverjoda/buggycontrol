1c165e36947a0af7c88a54e01e0caad9c9cd0053
silverjoda <heizen.sh@gmail.com>
2022-02-17 12:00:28 +0100

    Debug ss training

--- src/opt/train_buggy_ss_trajectory_follower.py
+++ src/opt/train_buggy_ss_trajectory_follower.py
@@ -22,9 +22,9 @@ class BuggySSTrajectoryTrainer:
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
-        self.x_file_path = os.path.join(dir_path, "X.pkl")
-        self.y_file_path = os.path.join(dir_path, "Y.pkl")
-        self.p_file_path = os.path.join(dir_path, "P.pkl")
+        self.x_file_path = os.path.join(dir_path, "X.npy")
+        self.y_file_path = os.path.join(dir_path, "Y.npy")
+        self.p_file_path = os.path.join(dir_path, "P.npy")
 
         self.traj_file_path = os.path.join(dir_path, "traj.pkl")
 
@@ -74,53 +74,54 @@ class BuggySSTrajectoryTrainer:
             # make dataset out of given traj
             x, y = self.make_trn_examples_from_traj(env, obs_list, act_list)
 
-            X.extend(x)
-            Y.extend(y)
-            P.extend([env.scaled_random_params] * len(x))
+            if len(x) < 100:
+                continue
+
+            X.append(np.array(x))
+            Y.append(np.array(y))
+            P.append(np.array([env.scaled_random_params] * len(x)))
 
         # Save as npy dataset
         X_arr = np.array(X)
         Y_arr = np.array(Y)
         P_arr = np.array(P)
 
-        pickle.dump(X_arr, open(self.x_file_path, "wb"))
-        pickle.dump(Y_arr, open(self.y_file_path, "wb"))
-        pickle.dump(P_arr, open(self.p_file_path, "wb"))
+        np.save(self.x_file_path, X_arr)
+        np.save(self.y_file_path, Y_arr)
+        np.save(self.p_file_path, P_arr)
 
         traj_list = []
         for x, y in zip(X, Y):
-            traj = Trajectory(obs=np.array(x), acts=y, infos=None)
+            traj = Trajectory(obs=x, acts=y[:-1], infos=None)
             traj_list.append(traj)
         pickle.dump(traj_list, open(self.traj_file_path, "wb"))
 
     def make_trn_examples_from_traj(self, env, obs_list, act_list):
         X = []
         Y = []
-        for current_state_idx in range(0, len(obs_list) - 200, self.config["traj_jump_dist"]):
-            rnd_offset = np.random.randint(1, 20)
 
-            # Iterate over trajectory and append every *dist* points
-            current_anchor_idx = current_state_idx
+        for current_state_idx in range(self.config["contiguous_traj_len"]):
             current_trajectory = []
-            for ti in range(current_state_idx + rnd_offset, len(obs_list)):
-                if self.get_dist_between_pts(obs_list[ti]["pos"], obs_list[current_anchor_idx]["pos"]) > self.config["traj_sampling_dist"]:
-                    wp = obs_list[ti]["pos"][0:2]
-                    current_trajectory.append(wp)
-                    current_anchor_idx = ti
-                if len(current_trajectory) == self.config["n_traj_pts"]: break
+            cum_wp_dist = 0
+            for ti in range(current_state_idx + 1, len(obs_list)):
+                cum_wp_dist += self.get_dist_between_pts(obs_list[ti]["pos"], obs_list[ti - 1]["pos"])
+                if cum_wp_dist > self.config["traj_sampling_dist"]:
+                    current_trajectory.append(obs_list[ti]["pos"][0:2])
+                    cum_wp_dist = 0
+                if len(current_trajectory) == self.config["n_waypoints"]:
+                    # Add state + trajectory observation to list
+                    state_vec = [obs_list[current_state_idx]["vel"][0],
+                                 obs_list[current_state_idx]["vel"][1],
+                                 obs_list[current_state_idx]["ang_vel"][2]]
+                    current_trajectory_buggy_frame = env.engine.transform_wp_to_buggy_frame(current_trajectory,
+                                                                                            obs_list[current_state_idx][
+                                                                                                "pos"],
+                                                                                            obs_list[current_state_idx][
+                                                                                                "ori_q"])
 
-            if len(current_trajectory) == self.config["n_traj_pts"]:
-                state_vec = [obs_list[current_state_idx]["vel"][0],
-                             obs_list[current_state_idx]["vel"][1],
-                             obs_list[current_state_idx]["ang_vel"][2]]
-                current_trajectory_buggy_frame = env.engine.transform_wp_to_buggy_frame(current_trajectory,
-                                                                                        obs_list[current_state_idx][
-                                                                                            "pos"],
-                                                                                        obs_list[current_state_idx][
-                                                                                            "ori_q"])
-
-                X.append(state_vec + list(current_trajectory_buggy_frame.reshape(-1)))
-                Y.append(act_list[current_state_idx])
+                    X.append(state_vec + list(current_trajectory_buggy_frame.reshape(-1)))
+                    Y.append(act_list[current_state_idx])
+                    break
 
             # DEBUG
             # env.engine.set_wp_visuals_externally(current_trajectory)
@@ -138,25 +139,36 @@ class BuggySSTrajectoryTrainer:
 
     def train_imitator_on_dataset(self):
         # Load dataset
-        X = pickle.load(open(self.x_file_path, "rb"))
-        Y = pickle.load(open(self.y_file_path, "rb"))
+        X = np.load(self.x_file_path)
+        Y = np.load(self.y_file_path)
+
+        n_traj = X.shape[0]
+        obs_dim = X.shape[2]
+        act_dim = Y.shape[2]
+        split_idx = int(n_traj * 0.8)
+        X_trn = X[0:split_idx].reshape([-1, obs_dim])
+        Y_trn = X[0:split_idx].reshape([-1, act_dim])
+        X_val = X[split_idx:].reshape([-1, obs_dim])
+        Y_val = Y[split_idx:].reshape([-1, act_dim])
 
         # Prepare policy and training
-        policy = MLP(obs_dim=X.shape[1], act_dim=2)
+        policy = MLP(obs_dim=obs_dim, act_dim=act_dim)
         optim = T.optim.Adam(params=policy.parameters(), lr=self.config['policy_lr'], weight_decay=self.config['w_decay'])
         lossfun = T.nn.MSELoss()
 
         for i in range(self.config["trn_iters"]):
-            rnd_indeces = np.random.choice(np.arange(len(X)), self.config["batchsize"], replace=False)
-            x = T.tensor(X[rnd_indeces], dtype=T.float32)
-            y = T.tensor(Y[rnd_indeces], dtype=T.float32)
+            rnd_indeces = np.random.choice(np.arange(len(X_trn)), self.config["batchsize"], replace=False)
+            x = T.tensor(X_trn[rnd_indeces], dtype=T.float32)
+            y = T.tensor(Y_trn[rnd_indeces], dtype=T.float32)
             y_ = policy(x)
             loss = lossfun(y_, y)
             loss.backward()
             optim.step()
             optim.zero_grad()
             if i % 50 == 0:
-                print("Iter {}/{}, loss: {}".format(i, self.config['trn_iters'], loss.data))
+                y_val_ = policy(T.tensor(X_val, dtype=T.float32))
+                val_loss = lossfun(y_val_, T.tensor(Y_val, dtype=T.float32))
+                print("Iter {}/{}, trn_loss: {}, val_loss: {}".format(i, self.config['trn_iters'], loss.data, val_loss.data))
         print("Done training, saving model")
         if not os.path.exists("agents"):
             os.makedirs("agents")
@@ -164,9 +176,9 @@ class BuggySSTrajectoryTrainer:
 
     def train_imitator_with_param_estimator_on_dataset(self):
         # Load dataset
-        X = pickle.load(open(self.x_file_path, "rb"))
-        Y = pickle.load(open(self.y_file_path, "rb"))
-        P = pickle.load(open(self.p_file_path, "rb"))
+        X = np.load(self.x_file_path)
+        Y = np.load(self.y_file_path)
+        P = np.load(self.p_file_path)
 
         # Prepare policy and training
         policy = MLP(obs_dim=X.shape[1] + self.config["n_latent_params"], act_dim=2)
@@ -215,5 +227,5 @@ class BuggySSTrajectoryTrainer:
 
 if __name__ == "__main__":
     bt = BuggySSTrajectoryTrainer()
-    bt.gather_ss_dataset()
-    #bt.train_imitator_on_dataset()
+    #bt.gather_ss_dataset()
+    bt.train_imitator_on_dataset()
