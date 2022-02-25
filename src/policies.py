987bf33633fabc08b85ff8bb44342a3714ecc29e
silverjoda <heizen.sh@gmail.com>
2022-02-25 16:25:47 +0100

    Add full traj tep

--- src/policies.py
+++ src/policies.py
@@ -3,6 +3,7 @@ import torch.nn.functional as F
 import torch as T
 from torch.nn.utils import weight_norm
 
+
 class SLP(nn.Module):
     def __init__(self, obs_dim, act_dim, hid_dim=128):
         super(SLP, self).__init__()
@@ -81,9 +82,9 @@ class RNN(nn.Module):
         fc2 = self.fc2(rnn_1)
         return fc2
 
-class TX(nn.Module):
+class TEPMLP(nn.Module):
     def __init__(self, obs_dim, act_dim, hid_dim=128):
-        super(TX, self).__init__()
+        super(TEPMLP, self).__init__()
         self.obs_dim = obs_dim
         self.act_dim = act_dim
         self.hid_dim = hid_dim
@@ -97,6 +98,54 @@ class TX(nn.Module):
 
         T.nn.init.xavier_uniform_(self.fc2.weight)
         self.fc2.bias.data.fill_(0.01)
+
+        T.nn.init.xavier_uniform_(self.fc3.weight)
+        self.fc3.bias.data.fill_(0.01)
+
+        self.nonlin = F.relu
+
+    def forward(self, x):
+        fc1 = self.nonlin(self.fc1(x))
+        fc2 = self.nonlin(self.fc2(fc1) + fc1)
+        out = self.fc3(fc2)
+        return out
+
+class TEPRNN(nn.Module):
+    def __init__(self, n_waypts, hid_dim=32, hid_dim_2=6):
+        super(TEPRNN, self).__init__()
+        self.n_waypts = n_waypts
+        self.hid_dim = hid_dim
+        self.hid_dim_2 = hid_dim_2
+
+        self.fc1 = T.nn.Linear(2, 4, bias=True)
+        self.rnn = T.nn.LSTM(input_size=4, hidden_size=self.hid_dim, num_layers=2, bias=True, batch_first=True, bidirectional=False)
+        self.fc2 = T.nn.Linear(self.hid_dim, self.hid_dim_2, bias=True)
+        self.fc3 = T.nn.Linear(self.n_waypts * self.hid_dim_2, 1, bias=True)
+
+        self.nonlin = F.relu
+
+    def forward(self, x):
+        x_reshaped = T.reshape(x, (len(x), self.n_waypts, 2))
+        fc1 = self.nonlin(self.fc1(x_reshaped))
+        rnn1 = self.rnn(fc1)
+        fc2 = self.nonlin(self.fc2(rnn1))
+        rnn1_reshaped = T.reshape(fc2, (len(x), self.n_waypts * self.hid_dim_2))
+        out = self.fc3(rnn1_reshaped)
+        return out
+
+class TEPTX(nn.Module):
+    def __init__(self, n_waypts, embed_dim, num_heads, kdim):
+        super(TEPTX, self).__init__()
+        self.n_waypts = n_waypts
+
+        self.fc1 = T.nn.Linear(2, embed_dim, bias=True)
+        self.fc_key = T.nn.Linear(embed_dim, kdim)
+        self.fc_value = T.nn.Linear(embed_dim, kdim)
+
+        self.tx1 = T.nn.MultiheadAttention(embed_dim=embed_dim, num_heads=num_heads, kdim=32)
+        self.tx2 = T.nn.MultiheadAttention(embed_dim=embed_dim, num_heads=num_heads, kdim=32)
+
+        self.fc_final = T.nn.Linear(self.n_waypts * kdim, 1)
 
         T.nn.init.xavier_uniform_(self.fc3.weight)
         self.fc3.bias.data.fill_(0.01)
