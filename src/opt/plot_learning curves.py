269a937282dacc46b371adce8579492bb2c050c4
silverjoda <heizen.sh@gmail.com>
2022-03-28 18:24:14 +0200

    Fix engine 2

--- src/opt/plot_learning curves.py
+++ src/opt/plot_learning curves.py
@@ -3,26 +3,27 @@ import seaborn as sns
 import os
 import numpy as np
 import pandas as pd
+import os
+
 
 def plot_basic():
-    file_path_def = "/home/silverjoda/SW/buggy_ws/src/buggycontrol/src/opt/logs/def"
-    file_path_sym = "/home/silverjoda/SW/buggy_ws/src/buggycontrol/src/opt/logs/be2sym"
+    file_path_def = f"{os.path.expanduser('~')}/SW/buggy_ws/src/buggycontrol/src/opt/logs/be1"
+    file_path_sym = f"{os.path.expanduser('~')}/SW/buggy_ws/src/buggycontrol/src/opt/logs/be2"
 
     def_list = []
-    for i in range(5):
+    for i in range(4):
         fp = os.path.join(file_path_def, "{}".format(i), "evaluations.npz")
         data = np.load(fp)
-        def_list.append(np.mean(data.f.results, axis=1)[:49])
+        data_results = data.f.results
+        def_list.append(np.mean(data_results, axis=1)[:80])
     def_arr = np.mean(np.array(def_list), axis=0)
 
     sym_list = []
-    for i in range(5):
+    for i in range(4):
         fp = os.path.join(file_path_sym, "{}".format(i), "evaluations.npz")
         data = np.load(fp)
-        df = np.zeros(50)
-        df[np.arange(0,50,2)] = np.mean(data.f.results, axis=1)
-        df[np.arange(1,50,2)] = np.mean(data.f.results, axis=1)
-        sym_list.append(df[:49])
+        data_results = data.f.results
+        sym_list.append(np.mean(data_results, axis=1)[:80])
     sym_arr = np.mean(np.array(sym_list), axis=0)
 
     N = len(def_arr)
